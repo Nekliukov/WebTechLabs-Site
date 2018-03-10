d2423905bbd348d61d831004b3ec4ac2f0054819
Nekliukov <__>
2018-03-10 03:02:48 +0300

    LR2 Ver. 1.1

--- LR-2/LR2.php
+++ LR-2/LR2.php
@@ -1,14 +1,7 @@
 <?php //php 7.0.8
 
-echo "<body style=\"font-size:13px; background-color: black;\">";
-
-$arr = [ [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
-		     [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
-		     [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
-		     [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
-       ]; 
-
-for($i=0; $i<5; $i++){
+function init_array($arr){
+  for($i=0; $i<5; $i++){
        for($j=0; $j<3; $j++){
            for($k=0; $k<3; $k++){
                for($m=0; $m<2; $m++){
@@ -18,44 +11,49 @@ for($i=0; $i<5; $i++){
                }
            }
        }
+  }
+  return $arr;
 }
 
-for($i=0; $i<5; $i++){
-    $clr="";
-    switch ($i) {
-    case 0:
-        $clr="red";
-        break;
-    case 1:
-        $clr="blue";
-        break;
-    case 2:
-        $clr="green";
-        break;
-    case 3:
-        $clr="purple";
-        break;
-    case 4:
-        $clr="yellow";
-        break;
-    }
-       for($j=0; $j<3; $j++){
-           for($k=0; $k<3; $k++){
-               for($m=0; $m<2; $m++){
-                   for($n=0; $n<3; $n++){
-                        echo "<span style=\"color: $clr;\">{$arr[$i][$j][$k][$m][$n]} </span>";
-                   }
-                   echo "--";
-               }
-               echo "---";
-           }
-           echo "-----";
-       }
-    echo "<br/>";
+function show_array($arr){
+  for($i=0; $i<5; $i++){
+      $clr="";
+      switch ($i) {
+      case 0:
+          $clr="red";
+          break;
+      case 1:
+          $clr="blue";
+          break;
+      case 2:
+          $clr="green";
+          break;
+      case 3:
+          $clr="purple";
+          break;
+      case 4:
+          $clr="yellow";
+          break;
+      }
+         for($j=0; $j<3; $j++){
+             for($k=0; $k<3; $k++){
+                 for($m=0; $m<2; $m++){
+                     for($n=0; $n<3; $n++){
+                          echo "<span style=\"color: $clr;\">{$arr[$i][$j][$k][$m][$n]} </span>";
+                     }
+                     echo "--";
+                 }
+                 echo "---";
+             }
+             echo "-----";
+         }
+      echo "<br/>";
+  }
+  echo "<br/></br>";
 }
-echo "<br/></br>";
 
-for($i=0; $i<5; $i++){
+function get_result($arr){
+  for($i=0; $i<5; $i++){
        for($j=0; $j<3; $j++){
            for($k=0; $k<3; $k++){
                for($m=0; $m<2; $m++){
@@ -75,7 +73,23 @@ for($i=0; $i<5; $i++){
            echo "-----";
        }
     echo "<br/>";
+  }
 }
 
-echo "</div>";
+echo "<body style=\"font-size:11px; font-family: sans-serif; background-color: black;\">";
+
+$arr = [ [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
+     [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
+     [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
+     [[[[],[]], [[],[]], [[],[]]],  [[[], []], [[], []], [[], []]],  [[[],[]],[[],[]],[[],[]]]],
+       ]; 
+
+//Инициализация массива случайными числами от 0 до 99
+$arr = init_array($arr);
+//Вывод массива на экран
+show_array($arr);
+echo "<br/>";
+//Результат работы программы
+get_result($arr);
+
 ?>

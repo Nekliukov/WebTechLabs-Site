b088453437dd5e583c240dd3a402675c1ed270dc
Nekliukov <roman.nekliukov@gmail.com>
2018-04-04 22:44:06 +0300

    LR 5

---
+++ LR5/show_db.php
@@ -0,0 +1,28 @@
+<?php
+    require_once 'LR5.html';
+    require_once 'connection.php'; // подключаем скрипт
+    // подключаемся к серверу
+    $link = mysqli_connect($host, $user, $password, $database)
+    or die("Error " . mysqli_error($link));
+
+    $query = "SELECT * FROM email";
+
+        $result=mysqli_query($link,$query)
+        or die("Error ".mysqli_error($link)); // запрос на выборку
+
+        $rows = mysqli_num_rows($result); // количество полученных строк
+        
+        if($result){
+            echo "<table><tr><th>E-mails</th></tr>";
+            for ($i = 0 ; $i < $rows ; ++$i){
+                $row = mysqli_fetch_row($result);
+                echo "<tr>";
+                for ($j = 0 ; $j < 1 ; ++$j)
+                    echo "<td>$row[$j]</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            mysqli_free_result($result);
+        }
+    mysqli_close($link);
+?>
